afc60bb370d69e90ada5aa8565d362c164ba0878
Kamil Gniazdowski <110768094+Dredro@users.noreply.github.com>
2025-01-22 18:47:32 +0100

    adfds

--- routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getPosts, createPost, likePost } = require('../controllers/postController');
-const router = express.Router();
-
-router.get('/', getPosts);
-router.post('/', createPost);
-router.post('/:postId/like', likePost);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { followUser } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/follow', followUser);
-
-module.exports = router;
